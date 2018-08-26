f3358b6394c421caf4c2287891d977ee61ce3bcd
mayukoro <vc.mii.tantan@gmail.com>
2018-08-26 21:49:55 +0900

    add switch

--- omikuji.js
+++ omikuji.js
@@ -4,12 +4,16 @@
     var btn=document.getElementById('btn')
     btn.addEventListener('click',function(){
         var n=Math.floor(Math.random()*3);
-        if(n===0){
-            this.textContent='大吉';
-        }else if(n===1){
-            this.textContent='中吉';
-        }else{
-            this.textContent='凶';
+        switch(n){
+            case 0:
+                this.textContent='大吉';
+                break;
+            case 1:
+                this.textContent='中吉';
+                break;
+            case 2:
+                this.textContent='凶';
+                break;
         }
         
     });
